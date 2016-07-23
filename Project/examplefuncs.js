8919e93fb35c64419451e56e540288ebe171af80
Keller Jordan <kellerjordan1998@gmail.com>
2016-07-22 21:27:53 -0700

    added project files to github pages

---
+++ Project/examplefuncs.js
@@ -0,0 +1,69 @@
+// drawing axes with length bound
+var bound=10;
+var drawList=[new line([-.1*bound,0,0],[1.1*bound,0,0]), new line([0,-.1*bound,0],[0,1.1*bound,0]), new line([0,0,-.1*bound],[0,0,1.1*bound])];
+
+// var vect1=[10,6,7], vect2=[-9,10,8];
+// var vect3=vCrossProduct(vect1,vect2);
+// drawList.push(new line(ogn,vect3));
+// drawList.push(new pGram(vect1,vect2));
+// arb=50;
+// for(var i=0;i<arb;i++){drawList.push(new line(ogn,rotatePoint([100,0,0],i*Math.PI/(arb/2),0)))}
+// square(ogn,30);fractal(ogn,10);
+
+// function f(x,y){
+
+// }
+
+// function graph2(func,z){
+
+// }
+
+// function graph3(func){
+// 	if(func.length==1){graph2(func,0);}
+// }
+
+function plotPath(func,step,max){
+	var curr;
+	var prev=func(0);
+	for(var t=step;t<max;t+=step){
+		curr=func(t);
+		drawList.push(new line(prev,vSubtract(curr,prev)));
+		prev=curr;
+	}
+}
+
+function plotSurface(func,max){
+	var step=max/20;
+	for(var s=-1*max;s<max;s+=step){
+		var curr;
+		var prev=func(s,-1*max);
+		for(var t=-1*max+step;t<max;t+=step){
+			curr=func(s,t);
+			if(isValid(prev)&&isValid(curr)){drawList.push(new line(prev,vSubtract(curr,prev)));}
+			// drawList.push(new line(prev,vSubtract(curr,prev)));
+			prev=curr;
+		}
+		var prev=func(-1*max,s);
+		for(var t=-1*max+step;t<max;t+=step){
+			curr=func(t,s);
+			if(isValid(prev)&&isValid(curr)){drawList.push(new line(prev,vSubtract(curr,prev)));}
+			// drawList.push(new line(prev,vSubtract(curr,prev)));
+			prev=curr;
+		}
+	}
+}
+
+function isValid(point){
+	for(var i in point){if(isNaN(point[i])){return false;}}
+	return true;
+}
+
+// var cvect1=[2,2];
+// var cvect2=[2,5];
+// cPlot(cvect1);
+// cPlot(cvect2);
+// cPlot(cMultiply(cvect1,cvect2));
+
+plotSurface(function(x,y){return [x,y,Math.cos(x+y)+x*x/6+y*y/6]},10);
+// plotSurface(function(x,y){return [x,y,x*x+y*y+2*x*y]},2);
+// drawList.push(new line(ogn,[8,12,8],"red"));
---
+++ Project/vector.js
@@ -0,0 +1,72 @@
+//could add checks for vectors same dimension
+function vAdd(v1,v2){var result=[];for(var i in v1){result.push(v1[i]+v2[i])}return result}
+
+function vSubtract(v1,v2){var result=[];for(var i in v1){result.push(v1[i]-v2[i])}return result}
+
+function vMultiply(v,s){var result=[];for(var i in v){result.push(s*v[i])}return result}
+
+function vDotProduct(v1,v2){var result=0;for(var i in v1){result+=v1[i]*v2[i]}return result}
+
+function vCrossProduct(vArr){
+	var r=[];
+	for(var i = 0; i<vArr.length+1; i++){
+		var d = mDeterminant(mSplice(vArr,i));
+		if(i % 2){d *= -1}
+		r.push(d);
+	}
+	return r;
+}
+
+function vDistance(v){return Math.sqrt(vDotProduct(v,v))}
+
+function vPolar(v){
+	var p=[Math.sqrt(v[0]*v[0]+v[1]*v[1]),Math.atan2(v[1],v[0])];
+	for(var i=2;i<v.length;i++){p.push(v[i])}
+	return p;
+}
+
+function vRectangular(v){
+	var r=[v[0]*Math.cos(v[1]),v[0]*Math.sin(v[1])];
+	for(var i=2;i<v.length;i++){r.push(v[i])}
+	return r;
+}
+
+class pGram{
+	constructor(v1,v2){
+		this.line1=new line(ogn,v1);
+		this.line2=new line(ogn,v2);
+		this.line3=new line(v1,v2);
+		this.line4=new line(v2,v1);
+	}
+	draw(){
+		this.line1.draw();
+		this.line2.draw();
+		this.line3.draw();
+		this.line4.draw();
+	}
+}
+
+
+function fractal(c,r){
+	square(c,r);
+	if(r<30*Math.pow(3,-3)){return}
+	fractal([c[0]-2*r,c[1]-2*r,c[2]],r/3);
+	fractal([c[0],c[1]-2*r,c[2]],r/3);
+	fractal([c[0]+2*r,c[1]-2*r,c[2]],r/3);
+	fractal([c[0]+2*r,c[1],c[2]],r/3);
+	fractal([c[0]+2*r,c[1]+2*r,c[2]],r/3);
+	fractal([c[0],c[1]+2*r,c[2]],r/3);
+	fractal([c[0]-2*r,c[1]+2*r,c[2]],r/3);
+	fractal([c[0]-2*r,c[1],c[2]],r/3);
+}
+
+function square(c,s){
+	var p1=[c[0]-s,c[1]-s,c[2]];
+	var p2=[c[0]+s,c[1]-s,c[2]];
+	var p3=[c[0]+s,c[1]+s,c[2]];
+	var p4=[c[0]-s,c[1]+s,c[2]];
+	drawList.push(new line(p1,vSubtract(p2,p1)));
+	drawList.push(new line(p2,vSubtract(p3,p2)));
+	drawList.push(new line(p3,vSubtract(p4,p3)));
+	drawList.push(new line(p4,vSubtract(p1,p4)));
+}
